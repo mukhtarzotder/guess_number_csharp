23698ea0fc0e5b8b0802fe12970d7750fd70abfc
mukhtar-zotder <mukhtarzotder@gmail.com>
2020-08-07 13:46:29 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var random = new Random();
+
+            var secret = Convert.ToString(random.Next(99, 103));
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("Guess a number ");
+                var guess = (Console.ReadLine());
+                if (guess == secret)
+                {
+                    Console.WriteLine("you won! ");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("you lost! ");
+        }
+    }
+}
